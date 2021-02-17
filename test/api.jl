30f73552fb17803695599d9d9c2f40d7019e5389
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-02-17 15:24:59 +1300

    more codecov

--- test/api.jl
+++ test/api.jl
@@ -3,6 +3,9 @@ import EarlyStopping: done, update, update_training
 # dummy criterion: stop when loss = training loss
 struct Dummy <: StoppingCriterion end
 
+@test update_training(Dummy(), 1.0) === nothing
+@test update_training(Dummy(), 1.0, 42.0) == 42.0
+
 update_training(c::Dummy, loss) = (training=loss, loss=nothing)
 update_training(c::Dummy, loss, state) = (training=loss, loss=state.loss)
 update(c::Dummy, loss, state) = (training=state.training, loss=loss)
